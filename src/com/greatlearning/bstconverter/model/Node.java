003d63c8a56edadcbbfb058258f21a22e6736099
Pawar677 <87629657+Pawar677@users.noreply.github.com>
2021-11-14 23:12:34 +0530

    Add files via upload

---
+++ src/com/greatlearning/bstconverter/model/Node.java
@@ -0,0 +1,17 @@
+package com.greatlearning.bstconverter.model;
+
+public class Node {
+	private int data;
+    public Node left, right;
+
+    public Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+
+    public int getData() {
+        return data;
+    }
+}
+
+
